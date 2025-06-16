c0374b13be366f00bde09a06b6e246a4b9380ec2
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2025-06-16 12:34:46 +0200

    Create ScanScreen.kt, DetailScreen.kt, FoodListScreen.kt & BeautyListScreen.kt

---
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/ui/screen/BeautyListScreen.kt
@@ -0,0 +1,20 @@
+package de.syntax_institut.androidabschlussprojekt.ui.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BeautyListScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Kosmetik Sammlung")
+    }
+}
