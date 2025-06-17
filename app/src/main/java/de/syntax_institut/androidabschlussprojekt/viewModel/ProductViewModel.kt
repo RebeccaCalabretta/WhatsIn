4861a2ff740eaa47983499a1171e07aa9c21f6bf
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2025-06-17 20:10:04 +0200

    Create scannedEAN state and startScan() in ProductViewModel

--- app/src/main/java/de/syntax_institut/androidabschlussprojekt/viewModel/ProductViewModel.kt
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/viewModel/ProductViewModel.kt
@@ -11,7 +11,14 @@ class ProductViewModel : ViewModel() {
     private val _selectedProduct = MutableStateFlow<Product?>(null)
     val selectedProduct = _selectedProduct.asStateFlow()
 
+    private val _scannedEan = MutableStateFlow<String?>(null)
+    val scannedEan = _scannedEan.asStateFlow()
+
     fun selectProduct(product: Product) {
         _selectedProduct.value = product
+    }
+
+    fun startScan(ean: String) {
+        _scannedEan.value = ean
     }
 }
