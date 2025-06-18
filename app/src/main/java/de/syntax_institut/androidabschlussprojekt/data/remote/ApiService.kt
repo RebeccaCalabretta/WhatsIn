e13f2db901d2df99b41c90b34464b8130dbb1094
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2025-06-18 10:51:13 +0200

    Create ApiService and set up Retrofit with Moshi

---
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/data/remote/ApiService.kt
@@ -0,0 +1,18 @@
+package de.syntax_institut.androidabschlussprojekt.data.remote
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+const val BASE_URL = "https://world.openfoodfacts.org/api/v2/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
