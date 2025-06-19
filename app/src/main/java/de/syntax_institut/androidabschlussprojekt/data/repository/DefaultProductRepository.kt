f9a345ba36c2c6c58bb8372d7cdb4e0176a60085
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2025-06-19 11:55:56 +0200

    Throw ProductException in ProductRepository

--- app/src/main/java/de/syntax_institut/androidabschlussprojekt/data/repository/DefaultProductRepository.kt
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/data/repository/DefaultProductRepository.kt
@@ -2,15 +2,21 @@ package de.syntax_institut.androidabschlussprojekt.data.repository
 
 import de.syntax_institut.androidabschlussprojekt.data.remote.ApiService
 import de.syntax_institut.androidabschlussprojekt.data.remote.model.toProduct
+import de.syntax_institut.androidabschlussprojekt.error.ProductError
+import de.syntax_institut.androidabschlussprojekt.error.ProductException
 import de.syntax_institut.androidabschlussprojekt.model.Product
 
 class DefaultProductRepository(
     private val api: ApiService
 ) : ProductRepository {
-    override suspend fun fetchProductByBarcode(barcode: String): Product? {
+    override suspend fun fetchProductByBarcode(barcode: String): Product {
         val response = api.getProductByBarcode(barcode)
         val productDto = response.product
 
-        return productDto?.toProduct() ?: throw Exception("Kein Produkt mit dem Code $barcode gefunden.")
+        if (productDto == null) {
+            throw ProductException(ProductError.NOT_FOUND)
+        }
+
+        return productDto.toProduct()
     }
 }
