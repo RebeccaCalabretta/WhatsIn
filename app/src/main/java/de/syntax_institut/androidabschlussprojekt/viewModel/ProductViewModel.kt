adee5b4ec4917fbfa3d650ab923d683c52bc4eba
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2025-06-19 12:22:34 +0200

    Create ProductError state & clearProductError() and add to function

--- app/src/main/java/de/syntax_institut/androidabschlussprojekt/viewModel/ProductViewModel.kt
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/viewModel/ProductViewModel.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import de.syntax_institut.androidabschlussprojekt.data.repository.ProductRepository
+import de.syntax_institut.androidabschlussprojekt.error.ProductError
+import de.syntax_institut.androidabschlussprojekt.error.ProductException
 import de.syntax_institut.androidabschlussprojekt.model.Product
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -20,6 +22,9 @@ class ProductViewModel(
     private val _scannedBarcode = MutableStateFlow<String?>(null)
     val scannedBarcode = _scannedBarcode.asStateFlow()
 
+    private val _productError = MutableStateFlow<ProductError?>(null)
+    val productError = _productError.asStateFlow()
+
     fun selectProduct(product: Product) {
         _selectedProduct.value = product
         Log.d("ProductViewModel", "Produkt ausgewählt: ${product.name}")
@@ -31,6 +36,10 @@ class ProductViewModel(
 
     }
 
+    fun clearProductError() {
+        _productError.value = null
+    }
+
     fun getProductByBarcode(barcode: String) {
         Log.d("ProductViewModel", "API-Aufruf für Barcode: $barcode")
 
@@ -38,12 +47,16 @@ class ProductViewModel(
             try {
                 val product = repository.fetchProductByBarcode(barcode)
                 _selectedProduct.value = product
+                clearProductError()
                 Log.d("ProductViewModel", "Produkt geladen: ${product?.name}")
 
-            } catch (e: Exception) {
+            } catch (e: ProductException) {
                 _selectedProduct.value = null
+                _productError.value = e.error
                 Log.e("ProductViewModel", "Fehler beim Laden des Produkts: ${e.message}")
             }
         }
     }
+
+
 }
