1e90efd44668514c7e9712100a21b45823437db3
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2025-06-23 11:34:21 +0200

    Add StateFlow for ActiveFilter with init and prepare loadFilterFromDataStore

--- app/src/main/java/de/syntax_institut/androidabschlussprojekt/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/viewmodel/SettingsViewModel.kt
@@ -1,7 +1,25 @@
 package de.syntax_institut.androidabschlussprojekt.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import de.syntax_institut.androidabschlussprojekt.model.ActiveFilter
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class SettingsViewModel : ViewModel() {
+    private val _activeFilter = MutableStateFlow(ActiveFilter())
+    val activeFilter: StateFlow<ActiveFilter> = _activeFilter
 
+    init {
+        Log.d("SettingsViewModel", "SettingsViewModel gestartet – Filter wird vorbereitet")
+        viewModelScope.launch {
+            loadFilterFromDataStore()
+        }
+    }
+
+    private fun loadFilterFromDataStore() {
+        Log.d("SettingsViewModel", "loadFilterFromDataStore() aufgerufen")
+    }
 }
