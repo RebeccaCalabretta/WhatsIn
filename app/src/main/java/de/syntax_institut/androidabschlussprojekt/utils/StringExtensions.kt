b0960ca3d973a19b71c2cbaed30ed93aff11e74f
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2025-06-27 10:06:05 +0200

    Add formatTag to StringExtensions

--- app/src/main/java/de/syntax_institut/androidabschlussprojekt/utils/StringExtensions.kt
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/utils/StringExtensions.kt
@@ -1,4 +1,6 @@
 package de.syntax_institut.androidabschlussprojekt.utils
 
-fun String.capitalizeWords(): String =
-    split(" ").joinToString(" ") { it.replaceFirstChar { char -> char.uppercaseChar() } }
+fun String.formatTag(): String =
+    removePrefix("en:")
+        .replace("-", " ")
+        .replaceFirstChar { it.uppercaseChar() }
