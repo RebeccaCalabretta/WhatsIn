f5146f5a49257922ddbb87dc769b3aff621bc7dc
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2025-07-01 13:48:48 +0200

    Load scanned products in CollectionViewModel

--- app/src/main/java/de/syntax_institut/androidabschlussprojekt/viewmodel/CollectionViewModel.kt
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/viewmodel/CollectionViewModel.kt
@@ -1,15 +1,36 @@
 package de.syntax_institut.androidabschlussprojekt.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import de.syntax_institut.androidabschlussprojekt.data.local.model.ScannedProduct
+import de.syntax_institut.androidabschlussprojekt.data.repository.ProductRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class CollectionViewModel: ViewModel() {
+class CollectionViewModel(
+    private val repository: ProductRepository
+) : ViewModel() {
 
     private val _searchText = MutableStateFlow("")
     val searchText: StateFlow<String> = _searchText
 
+    private val _allProducts = MutableStateFlow<List<ScannedProduct>>(emptyList())
+    val allProducts: StateFlow<List<ScannedProduct>> = _allProducts
+
+    init {
+        loadProducts()
+    }
+
     fun updateSearchText(newText: String) {
         _searchText.value = newText
+    }
+
+    fun loadProducts() {
+        viewModelScope.launch {
+            repository.getScannedProducts().collect { products ->
+                _allProducts.value = products
+            }
+        }
     }
 }
